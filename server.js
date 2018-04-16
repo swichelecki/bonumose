f88ae2e813b11f305fbcf46de42b8b1610e4b7bb
Steve Wichelecki <swichelecki@gmail.com>
2018-04-16 09:45:32 -0500

    route fix attempt 2 4/16

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '/dist')));
+app.use(express.static(path.resolve(__dirname, 'dist')));
 
 //app.use(express.static(__dirname + '/dist'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/dist', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
   //res.sendFile(path.resolve(__dirname, 'index.html'))
 });
 
